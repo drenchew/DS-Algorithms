6e3ac815ebfe1eee0cc9568eb6ee13b8db8dde7a
Dre <alekdre001@gmail.com>
2023-07-20 01:44:26 +0300

    rep

--- Algorithms/Main.cpp
+++ Algorithms/Main.cpp
@@ -2,32 +2,11 @@
 #include <stack>
 #include<string>
 
-#include"BST.h"
-
-
-using namespace std;
+#include"BST.h" //Binary Search Tree
 
 
 int main()
 {
-	//while (true)
-	{
-		BST<string> tree;
-
-		//BST<int> bs;
-		
-
-		for (int i = 10; i > 0; --i)
-		{
-			tree.insert(to_string(i));
-
-		}
-		for (auto it : tree)
-		{
-			cout << it;
-		}
-		///tree.print();
-	}
 
 	return 0;
 }
